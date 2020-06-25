0a3b29dcca05fca7fac43198bb93c871cf55115d
Ruslan Vaisov <ruslan.vaisov.fs@gmail.com>
2020-06-25 11:52:14 +0500

    phone call added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,53 +1,55 @@
-import React from "react";
-import "../assets/Footer.scss";
-import map from "../img/map.jpg";
-import compass from "../img/compass-black.png";
+import React from 'react';
+import '../assets/Footer.scss';
+import map from '../img/map.jpg';
+import compass from '../img/compass-black.png';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-top">
-        <div className="footer-top-left">
-          <div className="footer-compass">
-            <img src={compass} alt="компас" />
+    <footer className='footer'>
+      <div className='footer-top'>
+        <div className='footer-top-left'>
+          <div className='footer-compass'>
+            <img src={compass} alt='компас' />
           </div>
-          <div className="footer-contacts">
+          <div className='footer-contacts'>
             <h1>Контакты</h1>
-            <h1>(90) 9867674</h1>
+            <h1>
+              <a href='tel:+998909867674'>+998909867674</a>
+            </h1>
           </div>
-          <div className="footer-social-media">
+          <div className='footer-social-media'>
             <a
-              href="https://www.facebook.com/olejka.fs"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://www.facebook.com/olejka.fs'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               Facebook
             </a>
             <a
-              href="https://www.instagram.com/oleg_suz/"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://www.instagram.com/oleg_suz/'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               Instagram
             </a>
             <a
-              href="https://t.me/oleg_suz"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://t.me/oleg_suz'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               Telegram
             </a>
           </div>
         </div>
-        <div className="footer-top-right">
-          <img src={map} width={"80%"} alt="карта" />
+        <div className='footer-top-right'>
+          <img src={map} width={'80%'} alt='карта' />
         </div>
       </div>
-      <div className="footer-bottom">
+      <div className='footer-bottom'>
         <p>© 2020</p>
         <p>
-          Дизайн и разработка студии{" "}
-          <a href="http://aiso.uz/" target="_blank" rel="noopener noreferrer">
+          Дизайн и разработка студии{' '}
+          <a href='http://aiso.uz/' target='_blank' rel='noopener noreferrer'>
             Aiso
           </a>
         </p>
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,60 +1,62 @@
-import React from "react";
-import "../assets/Showcase.scss";
-import shadow from "../img/showcase-shadow.png";
+import React from 'react';
+import '../assets/Showcase.scss';
+import shadow from '../img/showcase-shadow.png';
 // import scroll from "../img/scroll.gif";
 
 const Showcase = () => {
   return (
-    <section className="showcase">
-      <div className="container">
-        <div className="showcase-content">
-          <div className="showcase-shadow">
-            <img src={shadow} width={"250%"} alt="барбер в ташкенте" />
+    <section className='showcase'>
+      <div className='container'>
+        <div className='showcase-content'>
+          <div className='showcase-shadow'>
+            <img src={shadow} width={'250%'} alt='барбер в ташкенте' />
           </div>
           {/* <div className="showcase-scroll">
               <img src={scroll} alt="скрол вниз" />
             </div> */}
-          <div className="showcase-appointment">
+          <div className='showcase-appointment'>
             <a
-              href="https://t.me/oleg_suz"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://t.me/oleg_suz'
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              {" "}
-              — ЗАПИСАТЬСЯ —{" "}
+              {' '}
+              — ЗАПИСАТЬСЯ —{' '}
             </a>
           </div>
-          <div className="showcase-bottom">
-            <div className="showcase-left">
-              <img src={shadow} width={"65px"} alt="барберская иконка" />
+          <div className='showcase-bottom'>
+            <div className='showcase-left'>
+              <img src={shadow} width={'65px'} alt='барберская иконка' />
               <h1>
                 Лучшее место <br /> для ваших волос <br /> в вашем городе.
               </h1>
             </div>
-            <div className="showcase-right">
-              <div className="showcase-contact">
+            <div className='showcase-right'>
+              <div className='showcase-contact'>
                 <p>Ташкент, Чехов 38</p>
-                <p>(90) 986 76 74</p>
+                <p>
+                  <a href='tel:+998909867674'>+998909867674</a>
+                </p>
               </div>
-              <div className="showcase-social-media">
+              <div className='showcase-social-media'>
                 <a
-                  href="https://www.facebook.com/olejka.fs"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  href='https://www.facebook.com/olejka.fs'
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
                   Facebook
                 </a>
                 <a
-                  href="https://www.instagram.com/oleg_suz/"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  href='https://www.instagram.com/oleg_suz/'
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
                   Instagram
                 </a>
                 <a
-                  href="https://t.me/oleg_suz"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  href='https://t.me/oleg_suz'
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
                   Telegram
                 </a>
