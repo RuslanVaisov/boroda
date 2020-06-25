63b9c68f236acbf4cfdddf41d7f03eac42f1a8ab
Ruslan Vaisov <ruslan.vaisov.fs@gmail.com>
2020-06-25 22:24:35 +0500

    added gallery lightbox

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
-import Navbar from "./components/Navbar";
-import Showcase from "./components/Showcase";
-import Services from "./components/Services";
-import About from "./components/About";
-import Instagram from "./components/Instagram";
-import Footer from "./components/Footer";
+import React, { useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Showcase from './components/Showcase';
+import Services from './components/Services';
+import About from './components/About';
+import Instagram from './components/Instagram';
+import Footer from './components/Footer';
 
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SimpleReactLightbox from 'simple-react-lightbox';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 function App() {
   useEffect(() => {
@@ -21,91 +23,93 @@ function App() {
 
     gsap.registerPlugin(ScrollTrigger);
 
-    tl.from(".nav-logo", 3, {
-      y: "100vh",
+    tl.from('.nav-logo', 3, {
+      y: '100vh',
       opacity: 0,
       delay: 1,
       scale: 3,
-      ease: "power4.inOut",
+      ease: 'power4.inOut',
     }).from(
-      ".showcase, .nav-secondary",
+      '.showcase, .nav-secondary',
       3,
       {
-        y: "100vh",
+        y: '100vh',
         delay: 1,
-        ease: "power4.inOut",
+        ease: 'power4.inOut',
       },
       0.5
     );
 
     tl2
-      .from(".services-center-left", 1, {
+      .from('.services-center-left', 1, {
         scrollTrigger: {
-          trigger: ".services-center-left",
-          start: "top 100%",
-          end: "top 80%",
+          trigger: '.services-center-left',
+          start: 'top 100%',
+          end: 'top 80%',
           scrub: 1,
         },
-        x: "-100vh",
+        x: '-100vh',
         opacity: 0,
       })
       .from(
-        ".services-center-right",
+        '.services-center-right',
         1,
         {
           scrollTrigger: {
-            trigger: ".services-center-right",
-            start: "top 100%",
-            end: "top 80%",
+            trigger: '.services-center-right',
+            start: 'top 100%',
+            end: 'top 80%',
             scrub: 1,
           },
-          x: "100vh",
+          x: '100vh',
           opacity: 0,
         },
         0
       )
-      .from(".services-bottom", 1, {
+      .from('.services-bottom', 1, {
         scrollTrigger: {
-          trigger: ".services-bottom",
-          start: "top 100%",
-          end: "top 80%",
+          trigger: '.services-bottom',
+          start: 'top 100%',
+          end: 'top 80%',
           scrub: 1,
         },
-        y: "20vh",
+        y: '20vh',
         opacity: 0,
       });
 
-    tl3.from(".about-main", 2, {
+    tl3.from('.about-main', 2, {
       scrollTrigger: {
-        trigger: ".about-main",
-        start: "top 100%",
-        end: "top 80%",
+        trigger: '.about-main',
+        start: 'top 100%',
+        end: 'top 80%',
         scrub: 1,
       },
-      y: "30vh",
+      y: '30vh',
       opacity: 0,
     });
 
-    tl4.from(".instagram-content img", 10, {
+    tl4.from('.instagram-content img', 100, {
       scrollTrigger: {
-        trigger: ".instagram-content",
-        start: "top 90%",
-        end: "top 80%",
+        trigger: '.instagram-content',
+        start: 'top 90%',
+        end: 'top 80%',
         scrub: 3,
       },
-      y: "100vh",
+      y: '100vh',
       opacity: 0,
       stagger: 0.5,
     });
   }, []);
 
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar />
       <Showcase />
       <Services />
       <About />
-      <Instagram />
+      <SimpleReactLightbox>
+        <Instagram />
+      </SimpleReactLightbox>
       <Footer />
     </div>
   );
--- src/components/Instagram.js
+++ src/components/Instagram.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../assets/Instagram.scss';
+
+import { SRLWrapper } from 'simple-react-lightbox';
+
 import instagram1 from '../img/instagram/1.jpg';
 import instagram2 from '../img/instagram/2.jpg';
 import instagram3 from '../img/instagram/3.jpg';
@@ -15,58 +18,212 @@ import instagram12 from '../img/instagram/12.jpg';
 import instagram13 from '../img/instagram/13.jpg';
 import instagram14 from '../img/instagram/14.jpg';
 import instagram15 from '../img/instagram/15.jpg';
+import instagram16 from '../img/instagram/16.jpg';
+import instagram17 from '../img/instagram/17.jpg';
+import instagram18 from '../img/instagram/18.jpg';
+import instagram19 from '../img/instagram/19.jpg';
+import instagram20 from '../img/instagram/20.jpg';
+import instagram21 from '../img/instagram/21.jpg';
+import instagram22 from '../img/instagram/22.jpg';
+import instagram23 from '../img/instagram/23.jpg';
+import instagram24 from '../img/instagram/24.jpg';
+import instagram25 from '../img/instagram/25.jpg';
+import instagram26 from '../img/instagram/26.jpg';
+import instagram27 from '../img/instagram/27.jpg';
+import instagram28 from '../img/instagram/28.jpg';
+import instagram29 from '../img/instagram/29.jpg';
+import instagram30 from '../img/instagram/30.jpg';
+import instagram31 from '../img/instagram/31.jpg';
+import instagram32 from '../img/instagram/32.jpg';
+import instagram33 from '../img/instagram/33.jpg';
+import instagram34 from '../img/instagram/34.jpg';
+import instagram35 from '../img/instagram/35.jpg';
+import instagram36 from '../img/instagram/36.jpg';
+import instagram37 from '../img/instagram/37.jpg';
+import instagram38 from '../img/instagram/38.jpg';
+import instagram39 from '../img/instagram/39.jpg';
+import instagram40 from '../img/instagram/40.jpg';
+import instagram41 from '../img/instagram/41.jpg';
+import instagram42 from '../img/instagram/42.jpg';
+import instagram43 from '../img/instagram/43.jpg';
+import instagram44 from '../img/instagram/44.jpg';
+import instagram45 from '../img/instagram/45.jpg';
+import instagram46 from '../img/instagram/46.jpg';
+import instagram47 from '../img/instagram/47.jpg';
+import instagram48 from '../img/instagram/48.jpg';
+import instagram49 from '../img/instagram/49.jpg';
 
 const Instagram = () => {
+  const options = {
+    settings: {
+      autoplaySpeed: 1500,
+      transitionSpeed: 900,
+    },
+    buttons: {
+      showDownloadButton: false,
+      showAutoplayButton: false,
+      showThumbnailsButton: false,
+      showFullscreenButton: false,
+    },
+    caption: {
+      showCaption: false,
+    },
+  };
+
   return (
     <section className='instagram'>
       <h1>Галерея</h1>
-      <div className='instagram-content'>
-        <div>
-          <img src={instagram1} alt='Инстаграм' />
+      <SRLWrapper options={options}>
+        <div className='instagram-content'>
+          <div>
+            <img src={instagram1} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram2} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram3} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram4} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram5} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram6} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram7} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram8} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram9} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram10} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram11} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram12} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram13} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram14} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram15} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram16} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram17} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram18} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram19} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram20} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram21} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram22} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram23} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram24} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram25} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram26} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram27} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram28} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram29} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram30} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram31} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram32} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram33} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram34} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram35} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram36} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram37} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram38} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram39} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram40} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram41} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram42} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram43} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram44} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram45} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram46} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram47} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram48} alt='Инстаграм' />
+          </div>
+          <div>
+            <img src={instagram49} alt='Инстаграм' />
+          </div>
         </div>
-        <div>
-          <img src={instagram2} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram3} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram4} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram5} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram6} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram7} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram8} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram9} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram10} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram11} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram12} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram13} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram14} alt='Инстаграм' />
-        </div>
-        <div>
-          <img src={instagram15} alt='Инстаграм' />
-        </div>
-      </div>
+      </SRLWrapper>
       <div className='instagram-text'>
         Перейти в{' '}
         <a
